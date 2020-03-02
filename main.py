6306f712d993d0ad4a65dc01775d3f45d8747c38
Leonard Colin <leooo.colin@gmail.com>
2020-03-02 16:39:32 +0100

    Create labyrinth logic

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ with open('map.txt', 'r') as file:
     print(file)
     for line in file:
         l = list(line)
+        l = ['m' if x == ' ' else x for x in l]
         l.remove('\n')
         lab.append(l)
 
-lab[1][0] = 'm'
+lab[1][5] = 'start'
+lab[13][5] = 'arrival'
 print(lab[1])
-print(lab)
+print(lab[13])
+
+# lab[1][0] = 'm'
+# print(lab[1])
+# print(lab)
+
+# for char in lab:
+#     if char == ' ':
+#         lab[char] = 'm'
+# print(lab)
