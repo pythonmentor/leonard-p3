d6564ed036c55bdce7e3fc08ac5ff08e6cadd204
Leonard Colin <leooo.colin@gmail.com>
2020-03-24 00:19:22 +0100

    Add final game logic and refactoring code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import os
 import random as r
 
 MACGYVER = 'M'
@@ -6,12 +5,17 @@ TOOLS = [
     'N',  # Needle
     'T',  # Tube
     'E'  # Ether
-     ]
+]
 GUARDIAN = 'G'
 FIRST_LINE = 1
 
-# Labyrinth = List[List[str]]
+state = {'labyrinth': None,
+         'picked_tools': [],
+         'game_running': True
+         }
 
+
+# Labyrinth = List[List[str]]
 # Position = Tuple[int, int]
 
 
@@ -27,7 +31,7 @@ def lab_surface(path):
     return lab
 
 
-LABYRINTH = lab_surface('map.txt')
+state['labyrinth'] = lab_surface('map.txt')
 
 
 # class CLI:
@@ -112,80 +116,50 @@ def get_macgyver_position(lab):
     return None
 
 
-def move_macgyver(lab):
-    """
-    Function that moves Mac Gyver in the labyrinth
-    :param lab: List[List[str]]
-    :return: List[List[str]]
-    """
+# Initialization
+Y_MAC_POS = pick_random_position(state['labyrinth'], FIRST_LINE)
+set_position(state['labyrinth'], FIRST_LINE, Y_MAC_POS, MACGYVER)
+
+X_GUARDIAN_POS = len(state['labyrinth']) - 2  # Second to last line
+Y_GUARDIAN_POS = pick_random_position(state['labyrinth'], X_GUARDIAN_POS)
+set_position(state['labyrinth'], X_GUARDIAN_POS, Y_GUARDIAN_POS, GUARDIAN)
+
+set_tool_positions(state['labyrinth'])
+
+
+# Game loop
+while state['game_running']:
+    lab = state['labyrinth']
+
+    lab_printer(lab)
     direction = input("Select direction 'Z, Q, S or D' : ")
-    x, y = get_macgyver_position(lab)
-    if direction == 'Z' and (lab[x - 1][y] == ' ' or lab[x - 1][y] in TOOLS):
-        lab[x][y] = ' '
-        lab[x - 1][y] = MACGYVER
-    elif direction == 'Q' and (lab[x][y - 1] == ' ' or lab[x][y - 1] in TOOLS):
-        lab[x][y] = ' '
-        lab[x][y - 1] = MACGYVER
-    elif direction == 'S' and (lab[x + 1][y] == ' ' or lab[x + 1][y] in TOOLS):
-        lab[x][y] = ' '
-        lab[x + 1][y] = MACGYVER
-    elif direction == 'D' and (lab[x][y + 1] == ' ' or lab[x][y + 1] in TOOLS):
-        lab[x][y] = ' '
-        lab[x][y + 1] = MACGYVER
+    if direction not in ['Z', 'Q', 'S', 'D']:
+        print("Please enter a valid direction")
+        continue
+
+    x1, y1 = get_macgyver_position(lab)  # Present Mac Gyver position
+
+    # https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+    x2, y2 = {
+        'Z': (x1 - 1, y1),
+        'Q': (x1, y1 - 1),
+        'S': (x1 + 1, y1),
+        'D': (x1, y1 + 1)
+        }[direction]                  # Future Mac Gyver position
+
+    if lab[x2][y2] in TOOLS:
+        state['picked_tools'].append(lab[x2][y2])
+
+    if lab[x2][y2] == ' ' or lab[x2][y2] in TOOLS:
+        lab[x2][y2] = MACGYVER
+        lab[x1][y1] = ' '
     else:
         print("Mac Gyver cannot go through walls !")
-    return lab
+        continue
 
-
-# def win_game(x, y, lab):
-#     x, y = get_macgyver_position(lab)
-#     if TOOLS not in lab and lab[x-1][y] == GUARDIAN:
-#         print("Congratulations, you win!")
-#     elif TOOLS not in lab and lab[x][y - 1] == GUARDIAN:
-#         print("Congratulations, you win!")
-#         elif lab[x + 1][y] == GUARDIAN:
-#             print("Congratulations, you win!")
-#         elif lab[x][y + 1] == GUARDIAN:
-#             print("Congratulations, you win!")
-
-
-
-
-
-
-Y_MAC_POS = pick_random_position(LABYRINTH, FIRST_LINE)
-set_position(LABYRINTH, FIRST_LINE, Y_MAC_POS, MACGYVER)
-
-X_GUARDIAN_POS = len(LABYRINTH) - 2  # Second to last line
-Y_GUARDIAN_POS = pick_random_position(LABYRINTH, X_GUARDIAN_POS)
-set_position(LABYRINTH, X_GUARDIAN_POS, Y_GUARDIAN_POS, GUARDIAN)
-
-
-set_tool_positions(LABYRINTH)
-
-lab_printer(LABYRINTH)
-
-move_macgyver(LABYRINTH)
-
-lab_printer(LABYRINTH)
-
-
-
-# def move():
-
-# start = lab.replace(randrange(len(lab[1])+1), MACGYVER)
-
-# print(start)
-
-
-# class MacGyver:
-#     """ Class that defines Mac Gyver movements """
-#     M = MacGyver()
-#     start_position = randrange[lab[1]]
-#
-#     def __init__(self):
-#         self.start = start_position
-#
-#
-# class Guardian:
-#     """ Class that defines Guardian position """
+    if GUARDIAN in [lab[x2 - 1][y2], lab[x2 + 1][y2], lab[x2][y2 + 1], lab[x2][y2 - 1]]:
+        if len(state['picked_tools']) == len(TOOLS):
+            print("Congratulation, you win!")
+        else:
+            print("Sorry, but you died!")
+        state['game_running'] = False
